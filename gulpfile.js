3b9b07bc465ec25d8d1099fee244f894b98a58a3
Florian Bruggisser <florian@nexpose.ch>
2018-03-06 11:36:26 +0100

    added gulp build file

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,73 @@ let uglify = require('gulp-uglify-es').default;
 let concat = require('gulp-concat-util');
 let watch = require('gulp-watch');
 
-gulp.task('build', function () {
-    gulp.src("./src/**/*")
-        .pipe(uglify())
-        .pipe(concat('immersive.min.js', {sep: ''}))
-        .pipe(gulp.dest('./resources/js/'));
+let sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+let rename = require("gulp-rename");
+let browserSync = require('browser-sync').create();
+
+let useref = require('gulp-useref');
+let gulpIf = require('gulp-if');
+
+let cssnano = require('gulp-cssnano');
+let imagemin = require('gulp-imagemin');
+let cache = require('gulp-cache');
+
+let del = require('del');
+
+gulp.task('images', function () {
+    return gulp.src('./app/img/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('./dist/img'))
 });
 
-gulp.task('develop', function () {
-    gulp.src("./src/**/*")
-        .pipe(concat('immersive.min.js', {sep: '\n\n'}))
-        .pipe(gulp.dest('./resources/js/'));
+// Compile SCSS
+gulp.task('css:compile', function () {
+    return gulp.src('./app/scss/**/*.scss')
+        .pipe(sass.sync({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
 });
 
-gulp.task('watch', function () {
-    watch('src/*', function () {
-        gulp.start('develop')
-    })
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function () {
+    return gulp.src([
+        './dist/css/*.css',
+        '!./dist/css/*.min.css'
+    ])
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
 });
+
+// CSS
+gulp.task('css', ['css:compile', 'css:minify']);
+
+// USER REF
+gulp.task('useref', function () {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
+});
+
+// VENDOR
+gulp.task('vendor', function () {
+    return gulp.src(['./app/vendor/**/*']).pipe(gulp.dest('./dist/vendor'));
+});
+
+// CLEAN
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+})
+
+gulp.task('build', ['clean:dist', 'useref', 'images', 'vendor']);
