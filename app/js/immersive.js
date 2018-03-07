8b7d169f28af4dc60105ff78a41413fb78ce71b7
Florian Bruggisser <florian@nexpose.ch>
2018-03-07 15:37:18 +0100

    implemented viewer controls

--- app/js/immersive.js
+++ app/js/immersive.js
@@ -1,19 +1,35 @@
-class ImmersiveSocieties {
-    init() {
-        pannellum.viewer('panorama', {
-            "type": "equirectangular",
-            "panorama": "https://pannellum.org/images/alma.jpg"
-        });
+function immersive() {
+    initAudio();
+    initPanorama();
+
+    function initAudio() {
+
     }
 
-    setupPanorama()
+    function initPanorama()
     {
-        pannellum.viewer('panorama', {
+        let viewer = pannellum.viewer('panorama', {
             "type": "equirectangular",
+            "showControls": false,
             "panorama": "https://pannellum.org/images/alma.jpg"
+        });
+
+        document.getElementById('fullscreen').addEventListener('click', function(e) {
+            viewer.toggleFullscreen();
+        });
+
+        document.getElementById('filter-traffic').addEventListener('click', function(e) {
+            viewer.setPitch(viewer.getPitch() + 10);
+        });
+
+        document.getElementById('filter-humans').addEventListener('click', function(e) {
+            viewer.setPitch(viewer.getPitch() + 10);
+        });
+
+        document.getElementById('filter-nature').addEventListener('click', function(e) {
+            viewer.setPitch(viewer.getPitch() + 10);
         });
     }
 }
 
-let immersiveSocieties = new ImmersiveSocieties();
-document.addEventListener('DOMContentLoaded', immersiveSocieties.init);
+document.addEventListener('DOMContentLoaded', immersive);
